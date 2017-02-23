a8003c9353305fdcd6c176d4b2659d3c759db973
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-22 16:39:11 -0800

    preliminary work to ind users and update them

---
+++ test/find-users.test.js
@@ -0,0 +1,10 @@
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
+// const assert = chai.assert;
+
+// const app = require('../../lib/app');
+// const mongoose = require('mongoose');
+
+// process.env.DB_URI = 'mongodb://localhost:27017/user-api-test';
+// require('../../lib/connection');
