42dc34e5429a316bb561e192c1f55ce08216a7d8
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-22 16:45:17 -0800

    continued work on update user unit test

--- test/update-user.test.js
+++ test/update-user.test.js
@@ -4,23 +4,39 @@ const assert = require('chai').assert;
 const User = require('../lib/models/user.model');
 const updater = require('../lib/update-user');
 
-describe.skip('update-user function', () => {
+describe.only('update-user function', () => {
     // let dummyJob = new Job()
-    let dummyUser = new User({
+    // let dummyUser = new User({
+    //     username: 'Testy',
+    //     hash: '$2a$08$F5f7nZtejb5o6N6kZ.XAP.kGlWCGoCnkOAgavSO/xd8LQq121wyEy',
+    //     job: {
+    //         start_date: '2016-02-10T00:48:31.406Z',
+    //         // job_name: 'something'/*{
+    //         //     jobType: 'Unskilled',
+    //         //     jobLevel: 'Entry',
+    //         //     monthlySalary: 1000,
+    //         //     promotionInterval: 1
+    //         // }*/
+    //     },
+    //     age: 18,
+    //     original_signup: '2016-02-10T00:48:31.406Z'
+    // });
+    let dummyUser = {
         username: 'Testy',
         hash: '$2a$08$F5f7nZtejb5o6N6kZ.XAP.kGlWCGoCnkOAgavSO/xd8LQq121wyEy',
         job: {
             start_date: '2016-02-10T00:48:31.406Z',
-            // job_name: 'something'/*{
-            //     jobType: 'Unskilled',
-            //     jobLevel: 'Entry',
-            //     monthlySalary: 1000,
-            //     promotionInterval: 1
-            // }*/
+            job_name: {
+                jobType: 'Unskilled',
+                jobLevel: 'Entry',
+                monthlySalary: 1000,
+                promotionInterval: 1
+            }
         },
         age: 18,
         original_signup: '2016-02-10T00:48:31.406Z'
-    });
+    };
+
 
     it('updates the age after one game year', () => {
         dummyUser = updater(new Date('2016-02-23'), dummyUser);
